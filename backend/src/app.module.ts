039ea9d6d23425fdfabbf2e82296d5746a7e7ddd
coc0a25 <cranemont@gmail.com>
2022-09-08 23:30:34 +0900

    feat: problem, testcase, submission 기본 설정 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
+import { TestcaseModule } from './testcase/testcase.module'
+import { ProblemService } from './problem/problem.service'
+import { ProblemController } from './problem/problem.controller'
+import { ProblemModule } from './problem/problem.module'
+import { SubmissionModule } from './submission/submission.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, TestcaseModule, ProblemModule, SubmissionModule],
+  controllers: [AppController, ProblemController],
+  providers: [AppService, ProblemService]
 })
 export class AppModule {}
---
+++ backend/src/testcase/testcase.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common'
+
+@Controller('testcase')
+export class TestcaseController {}
