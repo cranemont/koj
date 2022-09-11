6b2ebe0f640d85dc1b229d3c1b36c7fd3f9f79cf
mixxeo <alstj778@gmail.com>
2022-09-12 03:09:42 +0900

    chore: add rabbitmq module in submission module

--- backend/src/submission/submission.module.ts
+++ backend/src/submission/submission.module.ts
@@ -1,8 +1,27 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq'
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { SubmissionController } from './submission.controller'
 import { SubmissionService } from './submission.service'
 
 @Module({
+  imports: [
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        exchanges: [
+          {
+            name: 'submission-exchange',
+            type: 'direct',
+            options: { durable: true }
+          }
+        ],
+        uri: config.get('AMQP_URI'),
+        connectionInitOptions: { wait: false }
+      }),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [SubmissionController],
   providers: [SubmissionService]
 })
