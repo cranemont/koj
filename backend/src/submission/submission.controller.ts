312e16149c3bae914eb9c324c281a8f9d4ef2460
mixxeo <alstj778@gmail.com>
2022-09-12 03:48:49 +0900

    feat: implement create submission api

--- backend/src/submission/submission.controller.ts
+++ backend/src/submission/submission.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, InternalServerErrorException, Post } from '@nestjs/common'
+import { CreateSubmissionDto } from './dto/create-submission.dto'
+import { SubmissionService } from './submission.service'
 
 @Controller('submission')
-export class SubmissionController {}
+export class SubmissionController {
+    constructor(private readonly submissionService: SubmissionService) {}
+
+    @Post('problem/:problem_id/submission')
+    async createSubmission(
+      @Body() createSubmissionDto: CreateSubmissionDto
+    ) {
+      try {
+        await this.submissionService.createSubmission(
+          1,
+          "user-ip",
+          createSubmissionDto
+        )
+      } catch (error) {
+        throw new InternalServerErrorException()
+      }
+    }
+}
--- backend/src/submission/submission.service.ts
+++ backend/src/submission/submission.service.ts
@@ -1,4 +1,50 @@
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq'
 import { Injectable } from '@nestjs/common'
+import { Submission } from '@prisma/client'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CreateSubmissionDto } from './dto/create-submission.dto'
 
 @Injectable()
-export class SubmissionService {}
+export class SubmissionService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly amqpConnection: AmqpConnection
+  ) {}
+
+  async createSubmission(
+    userId: number,
+    ip: string,
+    createSubmissionDto: CreateSubmissionDto
+  ) {
+    const { languages } = await this.prisma.problem.findUnique({
+      where: { id: createSubmissionDto.problemId },
+      select: { languages: true }
+    })
+
+    if (!languages.includes(createSubmissionDto.language)) {
+      throw new Error(
+        `${createSubmissionDto.language} is not allowed`
+      )
+    }
+
+    const submission: Submission = await this.prisma.submission.create({
+      data: {
+        problem: {
+          connect: { id: createSubmissionDto.problemId }
+        },
+        code: createSubmissionDto.code,
+        language: createSubmissionDto.language,
+        ipAddr: ip
+      }
+    })
+
+    this.amqpConnection.publish(
+      'submission-exchange',
+      'submission',
+      submission,
+      { persistent: true }
+    )
+
+    return submission
+  }
+}
