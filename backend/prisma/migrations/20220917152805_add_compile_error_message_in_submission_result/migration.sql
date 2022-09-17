06d0c038e537d56d5902a7a88d974b6899ec5d0b
mixxeo <alstj778@gmail.com>
2022-09-18 01:29:27 +0900

    chore: update submision result table

---
+++ backend/prisma/migrations/20220917152805_add_compile_error_message_in_submission_result/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "submssion_result" ADD COLUMN     "compile_error_message" TEXT;
---
+++ backend/prisma/migrations/20220917152901_judge_result_field_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "submssion_result" ALTER COLUMN "judge_result" DROP NOT NULL;
