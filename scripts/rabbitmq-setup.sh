193f35f5b2f8b1ab95c3ce040eb5acd3a5094f8a
mixxeo <alstj778@gmail.com>
2022-09-17 22:03:12 +0900

    chore: add rabbitmq setup script

---
+++ scripts/rabbitmq-setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "AMQP_URI=\"amqp://skku:1234@localhost:5672/%2f\"" > .env
+
+# install rabbitmqadmin
+wget http://localhost:15672/cli/rabbitmqadmin
+chmod +x rabbitmqadmin
+
+# Make an Exchange
+rabbitmqadmin -u skku -p 1234 declare exchange name=judger-exchange type=direct
+
+# Make queues
+rabbitmqadmin -u skku -p 1234 declare queue name=submission-queue durable=true
+rabbitmqadmin -u skku -p 1234 declare queue name=result-queue durable=true
+
+# Make bindings
+rabbitmqadmin -u skku -p 1234 declare binding source=judger-exchange\
+                                destination_type=queue destination=submission-queue routing_key=submission
+rabbitmqadmin -u skku -p 1234 declare binding source=judger-exchange\
+                                destination_type=queue destination=result-queue routing_key=result
+
+rm rabbitmqadmin
+
