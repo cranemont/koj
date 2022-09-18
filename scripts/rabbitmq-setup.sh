237d65e54d90bf6858371422182cac7668d60a22
mixxeo <alstj778@gmail.com>
2022-09-18 21:26:55 +0900

    chore: add rabbitmq setup container

--- scripts/rabbitmq-setup.sh
+++ scripts/rabbitmq-setup.sh
@@ -1,22 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-echo "AMQP_URI=\"amqp://skku:1234@localhost:5672/%2f\"" > .env
+while ! nc -z "$RABBITMQ_HOST" "$RABBITMQ_PORT"; do sleep 3; done
+>&2 echo "rabbitmq is up - server running..."
 
 # install rabbitmqadmin
-wget http://localhost:15672/cli/rabbitmqadmin
+wget http://rabbitmq:15672/cli/rabbitmqadmin
 chmod +x rabbitmqadmin
 
 # Make an Exchange
-rabbitmqadmin -u skku -p 1234 declare exchange name=judger-exchange type=direct
+./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare exchange name=judger-exchange type=direct
 
 # Make queues
-rabbitmqadmin -u skku -p 1234 declare queue name=submission-queue durable=true
-rabbitmqadmin -u skku -p 1234 declare queue name=result-queue durable=true
+./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare queue name=result-queue durable=true
+./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare queue name=submission-queue durable=true
 
 # Make bindings
-rabbitmqadmin -u skku -p 1234 declare binding source=judger-exchange\
+./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare binding source=judger-exchange\
                                 destination_type=queue destination=submission-queue routing_key=submission
-rabbitmqadmin -u skku -p 1234 declare binding source=judger-exchange\
+./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare binding source=judger-exchange\
                                 destination_type=queue destination=result-queue routing_key=result
 
 rm rabbitmqadmin
