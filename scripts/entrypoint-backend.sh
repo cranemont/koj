1685a62153e3c098998fe017b8f7c1b5cd3b52a4
coc0a25 <cranemont@gmail.com>
2022-09-20 05:10:16 +0900

    fix: set pgbouncer connection in entrypoint

--- scripts/entrypoint-backend.sh
+++ scripts/entrypoint-backend.sh
@@ -12,9 +12,9 @@ if [ -n "$POSTGRES_USER" ] && \
    [ -n  "$POSTGRES_HOST" ] && \
    [ -n  "$POSTGRES_PORT" ]
 then
-    echo "DATABASE_URL=postgresql://"$POSTGRES_USER":"$POSTGRES_PASSWORD"@"$POSTGRES_HOST":"$POSTGRES_PORT"/"$POSTGRES_DB"?schema=public" > backend/.env
+    echo "DATABASE_URL=postgresql://"$POSTGRES_USER":"$POSTGRES_PASSWORD"@"$POSTGRES_HOST":"$POSTGRES_PORT"/"$POSTGRES_DB"?schema=public?pgbouncer=true" > backend/.env
 else
-    echo "DATABASE_URL=postgresql://skku:skku@postgres:5432/skku?schema=public" > backend/.env
+    echo "DATABASE_URL=postgresql://skku:skku@postgres:5432/skku?schema=public?pgbouncer=true" > backend/.env
     POSTGRES_HOST=postgres
     POSTGRES_PORT=5432
 fi
